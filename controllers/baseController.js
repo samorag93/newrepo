435c6e30348bb52396b3a93902fb4a018c19d5de
samorag93 <moronyag90@gmail.com>
2024-10-06 01:11:57 +0200

    ok

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,11 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities/");
+const baseController = {};
 
 baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+  console.log("Rendering home page..");
+  const nav = await utilities.getNav();
+  res.render("index", {title: "Home", nav});
+};
 
-module.exports = baseController
+module.exports = baseController;
+
